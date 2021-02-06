1ab592a716e55e9d7408ea597abde508649255f8
tubasthebest <tubasthebest@gmail.com>
2021-02-06 09:49:34 -0700

    post

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const inputCheck = require("./utils/inputCheck");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,16 +10,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
-const db = new sqlite3.Database('./db/election.db', err => {
+const db = new sqlite3.Database("./db/election.db", (err) => {
   if (err) {
     return console.error(err.message);
   }
 
-  console.log('Connected to the election database.');
+  console.log("Connected to the election database.");
 });
 
 // Get all candidates
-app.get('/api/candidates', (req, res) => {
+app.get("/api/candidates", (req, res) => {
   const sql = `SELECT * FROM candidates`;
   const params = [];
   db.all(sql, params, (err, rows) => {
@@ -28,14 +29,14 @@ app.get('/api/candidates', (req, res) => {
     }
 
     res.json({
-      message: 'success',
-      data: rows
+      message: "success",
+      data: rows,
     });
   });
 });
 
 // Get single candidate
-app.get('/api/candidate/:id', (req, res) => {
+app.get("/api/candidate/:id", (req, res) => {
   const sql = `SELECT * FROM candidates 
                WHERE id = ?`;
   const params = [req.params.id];
@@ -46,40 +47,29 @@ app.get('/api/candidate/:id', (req, res) => {
     }
 
     res.json({
-      message: 'success',
-      data: row
-    });
-  });
-});
-
-// Delete a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-  const sql = `DELETE FROM candidates WHERE id = ?`;
-  const params = [req.params.id];
-  db.run(sql, params, function(err, result) {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      return;
-    }
-
-    res.json({
-      message: 'successfully deleted',
-      changes: this.changes
+      message: "success",
+      data: row,
     });
   });
 });
 
 // Create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
-              VALUES (?,?,?)`;
+                VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
-  // ES5 function, not arrow function, to use `this`
+  // ES5 function, not arrow function, to use this
   db.run(sql, params, function (err, result) {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -94,26 +84,26 @@ app.post('/api/candidate', ({ body }, res) => {
   });
 });
 
+// Delete a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
 
-
-
-// // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
+    res.json({ message: "successfully deleted", changes: this.changes });
+  });
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
 });
 
+// Start server after DB connection
 db.on("open", () => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
